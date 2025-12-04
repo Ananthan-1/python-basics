33b0be2da7cbaf7b541214e6da77b9f5b3c89889
J Anantha Narayanan <j.k.ananthanarayanan@gmail.com>
2025-12-04 23:38:16 +0530

    Completed Day1 exercises and notebook

---
+++ exercises/.ipynb_checkpoints/day1-checkpoint.py
@@ -0,0 +1,27 @@
+# 1. Print name and age
+print("ANANTHA NARAYANAN",23)
+
+# 2. Write a program to check if a number is even or odd
+num=36
+if num%2==0:
+    print("EVEN")
+else:
+    print("ODD")
+
+    
+# 3. Print numbers from 1 to 20 using a loop
+for i in range(1,21):
+    print(i)
+
+    
+# 4. Write a function to find the square of a number
+def square(n):
+    return n*n
+
+print(square(10))
+
+
+# 5. Create a list of 5 items and print each item using a loop
+list1=["ANANTHAN",23,"INDIA","AI","FUTURE"]
+for i in list1:
+    print(i)
